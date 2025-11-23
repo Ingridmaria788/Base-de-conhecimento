8642ed40bb4b902e75a51873a7fb0641691ec5f8
Ingrid Maria Tenorio <84729297+Ingridmaria788@users.noreply.github.com>
2025-11-23 18:14:43 -0300

    Descrições aumentadas e script.js alterado para exibir horario de funcionamento e preço

--- script.js
+++ script.js
@@ -27,12 +27,23 @@ function renderizarCards(items) {
     for (const item of items) {
         const article = document.createElement("article");
         article.classList.add("card");
+
+        // Lógica para exibir o preço correto
+        let precoHtml = '';
+        if (item.preço_medio) {
+            precoHtml = `<p><strong>Preço Médio:</strong> ${item.preço_medio}</p>`;
+        } else if (item.preço_entrada) {
+            precoHtml = `<p><strong>Entrada:</strong> ${item.preço_entrada}</p>`;
+        }
+
         const imageUrl = item.image ? item.image : `https://source.unsplash.com/800x600/?${encodeURIComponent(item.nome)}`;
         article.innerHTML = `
             <img src="${imageUrl}" alt="${item.nome}" class="card-image"/>
             <div class="card-content">
                 <h2>${item.nome}</h2>
                 <p>${item.descricao}</p>
+                ${precoHtml}
+                <p>${item.horario_funcionamento}</p>
                 <a href="${item.maps_link}" target="_blank">Mais informações</a>
             </div>`;
         cardContainer.appendChild(article);
